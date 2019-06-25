cdb143ae6170666242bb1c2dd7b246aa0131e938
Yuriy Naydenov <ebceu44@gmail.com>
2019-06-25 11:01:48 +0300

    examples

---
+++ examples/old-style-bytes.ts
@@ -0,0 +1,12 @@
+import { address, publicKey, privateKey, keyPair } from '@waves/waves-crypto/bytes'
+
+const seed = 'secret seed'
+
+address(seed)
+
+publicKey(seed)
+
+privateKey(seed)
+
+keyPair(seed)
+
